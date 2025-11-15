97085af496caab0e18d3c11097863f20279b52a0
luiz <luizeduardotsdev@gmail.com>
2025-11-15 10:55:36 -0300

    feat: implementa o UUID no produto e salva no db

--- produtosapi/src/main/java/io/github/luizeduardotsdev/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/io/github/luizeduardotsdev/produtosapi/controller/ProdutoController.java
@@ -4,6 +4,8 @@ import io.github.luizeduardotsdev.produtosapi.model.Produto;
 import io.github.luizeduardotsdev.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/produto")
 public class ProdutoController {
@@ -17,6 +19,11 @@ public class ProdutoController {
     @PostMapping ("/salvar")
     public Produto salvar(@RequestBody Produto produto){
         System.out.println("Salvando produto: " + produto);
+
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+        produtoRepository.save(produto);
+
         return produto;
     }
 
--- produtosapi/src/main/java/io/github/luizeduardotsdev/produtosapi/repository/ProdutoRepository.java
+++ produtosapi/src/main/java/io/github/luizeduardotsdev/produtosapi/repository/ProdutoRepository.java
@@ -1,6 +1,7 @@
 package io.github.luizeduardotsdev.produtosapi.repository;
 
+import io.github.luizeduardotsdev.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProdutoRepository extends JpaRepository<ProdutoRepository,String> {
+public interface ProdutoRepository extends JpaRepository<Produto,String> {
 }
