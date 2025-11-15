a54d890db14c0a329a45106c7f26d5e667f7d517
luiz <luizeduardotsdev@gmail.com>
2025-11-15 10:50:32 -0300

    feat: implementa repository de produto no controller

--- produtosapi/src/main/java/io/github/luizeduardotsdev/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/io/github/luizeduardotsdev/produtosapi/controller/ProdutoController.java
@@ -1,11 +1,18 @@
 package io.github.luizeduardotsdev.produtosapi.controller;
 
 import io.github.luizeduardotsdev.produtosapi.model.Produto;
+import io.github.luizeduardotsdev.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/produto")
 public class ProdutoController {
+
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
 
     @PostMapping ("/salvar")
     public Produto salvar(@RequestBody Produto produto){
--- produtosapi/src/main/java/io/github/luizeduardotsdev/produtosapi/repository/ProdutoRepository.java
+++ produtosapi/src/main/java/io/github/luizeduardotsdev/produtosapi/repository/ProdutoRepository.java
@@ -2,5 +2,5 @@ package io.github.luizeduardotsdev.produtosapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Produto extends JpaRepository<Produto,String> {
+public interface ProdutoRepository extends JpaRepository<ProdutoRepository,String> {
 }
