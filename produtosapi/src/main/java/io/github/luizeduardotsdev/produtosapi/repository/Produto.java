220612bceb116f98ece994d518c1bb8c9d55b956
luiz <luizeduardotsdev@gmail.com>
2025-11-15 10:48:18 -0300

    feat: implementa repository de produto

---
+++ produtosapi/src/main/java/io/github/luizeduardotsdev/produtosapi/repository/Produto.java
@@ -0,0 +1,6 @@
+package io.github.luizeduardotsdev.produtosapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Produto extends JpaRepository<Produto,String> {
+}
